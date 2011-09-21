726d2a1d9f3d71d73d32771c8f35d9ea731728c5
Denver Coneybeare <denver.coneybeare@gmail.com>
2011-09-21 09:59:47 -0400

    Added About Activity

--- src/denver/sudoku/MainActivity.java
+++ src/denver/sudoku/MainActivity.java
@@ -1,13 +1,46 @@
 package denver.sudoku;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 
 public class MainActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        this.setContentView(R.layout.main);
+
+        final View continueButton = this.findViewById(R.id.continue_button);
+        continueButton.setOnClickListener(new ContinueOnClickListener());
+        final View newButton = this.findViewById(R.id.new_button);
+        newButton.setOnClickListener(new NewOnClickListener());
+        final View aboutButton = this.findViewById(R.id.about_button);
+        aboutButton.setOnClickListener(new AboutOnClickListener());
+        final View exitButton = this.findViewById(R.id.exit_button);
+        exitButton.setOnClickListener(new ExitOnClickListener());
+    }
+
+    private class AboutOnClickListener implements OnClickListener {
+        public void onClick(View v) {
+            final Intent i = new Intent(MainActivity.this, AboutActivity.class);
+            MainActivity.this.startActivity(i);
+        }
+    }
+
+    private class ContinueOnClickListener implements OnClickListener {
+        public void onClick(View v) {
+        }
+    }
+
+    private class ExitOnClickListener implements OnClickListener {
+        public void onClick(View v) {
+        }
+    }
+
+    private class NewOnClickListener implements OnClickListener {
+        public void onClick(View v) {
+        }
     }
 }
