2c066da50cd452dcee56c5c2b5631e716acbadb8
Denver Coneybeare <denver.coneybeare@gmail.com>
2011-09-23 09:40:58 -0400

    Added About activity

---
+++ src/denver/sudoku/AboutActivity.java
@@ -0,0 +1,18 @@
+/*
+ * AboutActivity.java
+ * By: Denver Coneybeare
+ * Sept 21, 2011
+ */
+package denver.sudoku;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AboutActivity extends Activity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about);
+    }
+    
+}
