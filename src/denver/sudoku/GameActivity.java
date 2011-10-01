e6a127e212d78a983b2702efe9bfa3592b83d523
Denver Coneybeare <dconeybeare@rim.com>
2011-10-01 19:31:46 -0400

    also search the region for available digits

--- src/denver/sudoku/GameActivity.java
+++ src/denver/sudoku/GameActivity.java
@@ -10,6 +10,8 @@ public class GameActivity extends Activity {
     public static final String KEY_DIFFICULTY =
         "denver.sudoku.GameActivity.KEY_DIFFICULTY";
 
+    private static int[][] REGIONS;
+
     public static final int DIFFICULTY_EASY = 0;
     public static final int DIFFICULTY_MEDIUM = 1;
     public static final int DIFFICULTY_HARD = 2;
@@ -41,6 +43,16 @@ public class GameActivity extends Activity {
         }
         for (int i = colIndex + 9; i < this.puzzle.length; i += 9) {
             final int value = this.puzzle[i];
+            if (value != 0) {
+                available[value - 1] = false;
+            }
+        }
+
+        // search the "region" for already used values
+        final int[] region = getRegionByIndex(index);
+        for (int i = region.length - 1; i >= 0; i--) {
+            final int curIndex = region[i];
+            final int value = this.puzzle[curIndex];
             if (value != 0) {
                 available[value - 1] = false;
             }
@@ -103,5 +115,44 @@ public class GameActivity extends Activity {
         }
         this.puzzle[index] = value;
         return true;
+    }
+
+    private static int[] getRegionByIndex(int index) {
+        final int[][] regions = getRegions();
+        for (int i = regions.length - 1; i >= 0; i--) {
+            final int[] region = regions[i];
+            for (int j = region.length - 1; j >= 0; j--) {
+                if (region[j] == index) {
+                    return region;
+                }
+            }
+        }
+        return null;
+    }
+
+    private static int[][] getRegions() {
+        int[][] regions = REGIONS;
+        if (regions == null) {
+            regions = new int[9][];
+            int regionsIndex = 0;
+
+            for (int row = 0; row < 3; row++) {
+                for (int col = 0; col < 3; col++) {
+                    final int[] region = new int[9];
+                    int startIndex = (row * 27) + (col * 3);
+                    int regionIndex = 0;
+                    for (int i = 1; i <= 9; i++) {
+                        region[regionIndex++] = startIndex++;
+                        if (i % 3 == 0) {
+                            startIndex += 6;
+                        }
+                    }
+                    regions[regionsIndex++] = region;
+                }
+            }
+            REGIONS = regions;
+        }
+
+        return regions;
     }
 }
